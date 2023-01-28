05821b4d8f673a4e7441256a6afb2febc51609f6
Julius Rio Shol <2021154969@ub.edu.bz>
2023-01-27 22:21:35 -0600

    question8 (circle)

--- circle.go
+++ circle.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -14,6 +15,18 @@ func (c Circle) area() float64 {
 	const Pi = 3.14159
 	return Pi * (math.Pow(c.radius, 2))
 }
+
+// 8.
+func (c Circle) perimeter() float64 {
+	const Pi = 3.14159
+	return 2 * (Pi * c.radius)
+}
 func main() {
 
+	c1 := Circle{
+		radius: 3,
+	}
+
+	fmt.Println(c1.area())
+	fmt.Println(c1.perimeter())
 }
