2a32cf77f2a0528b7120a52350a950635ee0e02b
Julius Rio Shol <2021154969@ub.edu.bz>
2023-01-27 22:00:46 -0600

    question7 (circle)

--- circle.go
+++ circle.go
@@ -1,12 +1,19 @@
 package main
 
+import (
+	"math"
+)
+
 // 6.
 type Circle struct {
 	radius float64
 }
 
 // 7.
-
+func (c Circle) area() float64 {
+	const Pi = 3.14159
+	return Pi * (math.Pow(c.radius, 2))
+}
 func main() {
 
 }
